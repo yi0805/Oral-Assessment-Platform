4c7f5aea259e94ea8d472854b964ba957e60901a
yi0805ian <ytan960@aucklanduni.ac.nz>
2026-03-14 16:52:54 +1300

    Set up styled-components

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
+import styled from "styled-components";
+
+import GlobalStyle from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
 function App() {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <>
+      <GlobalStyle />
+      <div>
+        <H1>Hello world!</H1>
+      </div>
+    </>
   );
 }
 
