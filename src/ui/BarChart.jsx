85f6c13a44a2aff1428eb8bc5fcac8a1a44ce501
TH0MASD0HMKE <cson299@aucklanduni.ac.nz>
2026-03-28 15:11:23 +1300

    Removed the y-axis for the cleaner visual

--- src/ui/BarChart.jsx
+++ src/ui/BarChart.jsx
@@ -2,7 +2,6 @@ import {
     BarChart,
     Bar,
     XAxis,
-    YAxis,
     Tooltip,
     ResponsiveContainer,
 } from "recharts";
@@ -13,7 +12,6 @@ function ScoreBarChart({ data }) {
         <ResponsiveContainer width="100%" height={200}>
             <BarChart data={data}>
                 <XAxis dataKey="name" />
-                <YAxis allowDecimals={false}/>
                 {/* shows the exact count for each grade */ }
                 <Tooltip />
                 <Bar dataKey="students" fill="var(--color-primary)" />
