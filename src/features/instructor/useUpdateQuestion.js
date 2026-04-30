2cd7d3d4b2af9c91995e8c6bf27284d98955f729
Joanne Chen <jehc820@aucklanduni.ac.nz>
2026-04-30 22:16:52 +1200

    Refactor Delete and Update Questions Hook

--- src/features/instructor/useUpdateQuestion.js
+++ src/features/instructor/useUpdateQuestion.js
@@ -1,15 +1,23 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 import { updateQuestion as updateQuestionApi } from "../../services/apiQuestion";
 import toast from "react-hot-toast";
 
 export function useUpdateQuestion() {
+  const queryClient = useQueryClient();
+
   const { mutateAsync: updateQuestion, isPending } = useMutation({
     mutationFn: ({ questionId, questionText }) =>
       updateQuestionApi(questionId, questionText),
 
-    onSuccess: (data) => {
+    onSuccess: (data, variables) => {
       toast.success(data?.message || "Question updated successfully.");
+
+      if (variables.assessmentConfigId) {
+        queryClient.invalidateQueries({
+          queryKey: ["questions", variables.assessmentConfigId],
+        });
+      }
     },
 
     onError: (error) => {
